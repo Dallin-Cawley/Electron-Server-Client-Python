9822c8139204b12f38682ec76baad791863bf91f
Dallin Cawley <dallin.cawley@gmail.com>
2020-07-29 12:29:10 -0600

    updated file path for download

--- python_scripts/ServerRequestHandler.py
+++ python_scripts/ServerRequestHandler.py
@@ -99,6 +99,8 @@ class ServerRequestHandlerSwitch:
                 try:
                     if file_name  == 'done':
                         break
+                    print("")
+                    file_name = file_name.replace('\\', '/')
                     file_name = Path(file_name)
                     opened_file = open(file_name, 'rb')
                     file_bytes = opened_file.read()
